f5c19e5814763235f8abebba2239f64135dc3188
Andy Becker <acbecker@gmail.com>
2014-03-10 18:33:20 -0700

    Print out an array of points

--- python/shapes2json.py
+++ python/shapes2json.py
@@ -1,18 +1,24 @@
-import re
+import re, sys
 import numpy as np
-infile = "shapes.txt"
+infile = sys.argv[1]
 filt = re.compile(r'^"?([^"]*)"?$')
 converter = lambda x: filt.match(x.strip()).group(1)
 data = np.recfromcsv(infile, delimiter=',')
 shapes = np.array(map(int, [converter(x) for x in data["shape_id"]]))
 lats = np.array(map(float, [converter(x) for x in data["shape_pt_lat"]]))
 lons = np.array(map(float, [converter(x) for x in data["shape_pt_lon"]]))
-idx = np.where(shapes == 10003553)
-print "{%s:" % (10003553),
-for la,lon in zip(lats[idx], lons[idx]): print "[%f,%f]," % (la,lon),
-print "}"
+idx = np.where(shapes == 41545540)[0]
 
-#import matplotlib.pyplot as plt
-#idx = np.where(shapes == 10003553)
-#plt.plot(lats[idx], lons[idx])
-#plt.show()
+if 0:
+	print '{"%s":' % (41545540),
+	for la,lon in zip(lats[idx], lons[idx]): print "[%f,%f]," % (la,lon),
+	print "}"
+else:
+	print "["
+	for i in idx:
+		print "{"
+		print '    name: "pt%d",' % (i)
+		print '    lng: %f,' % (lons[i])
+		print '    lat: %f,' % (lats[i])
+		print ' }, ',
+	print "]"
